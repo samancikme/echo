f32b51f54d48759979d8cceb926625b673fadff7
samancikme <samandarfarxotov513@gmail.com>
2025-04-23 15:28:10 +0500

    .

--- src/components/Main_load.jsx
+++ src/components/Main_load.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import image from '../assets/main_books.png'
+import React, { useState } from 'react';
+import image from '../assets/main_books.png';
+
 const Main_load = () => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
     return (
-        <div className='w-screen h-screen'>
-            <div className="">
-                <div className=''>
-                    <img src={image} alt="" />
+        <div className='w-screen h-screen bg-black'>
+            <div className="flex flex-col items-center justify-center h-full gap-4">
+                <div className='w-[300px] h-[300px] flex items-center justify-center'>
+                    {!imageLoaded && (
+                        <div className="w-full h-full bg-gray-800 rounded-md animate-pulse" />
+                    )}
+                    <img
+                        src={image}
+                        alt="main"
+                        className={`w-full h-full object-contain transition-opacity duration-300 ${imageLoaded ? 'opacity-100' : 'opacity-0'}`}
+                        onLoad={() => setImageLoaded(true)} />
                 </div>
+
                 <div className="flex flex-col gap-4">
                     <div className='text-4xl font-display text-center font-bold text-white'>
                         Learn a language easily with cards
@@ -17,7 +28,7 @@ const Main_load = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Main_load
+export default Main_load;
