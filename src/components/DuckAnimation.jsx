5cc03a72136d13079bd3c774833c69936884ed75
samancikme <samandarfarxotov513@gmail.com>
2025-04-27 15:25:45 +0500

    hello

--- src/components/DuckAnimation.jsx
+++ src/components/DuckAnimation.jsx
@@ -1,12 +1,12 @@
 import {Player} from '@lottiefiles/react-lottie-player';
-
+import sticker from '../public/animation/DUCK_HEY.json';
 function DuckAnimation() {
   return (
     <div className="w-60 h-60">
       <Player
         autoplay
         loop
-        src="/animation/DUCK_HEY.json" // JSON fayling manzili
+        src={sticker} // JSON fayling manzili
         style={{ height: '100%', width: '100%' }}
       />
     </div>
