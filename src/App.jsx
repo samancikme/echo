4c1bb708a1e420fa3bc0453995e4baabc85149ef
samancikme <samandarfarxotov513@gmail.com>
2025-04-27 21:47:08 +0500

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,39 @@ import Home from './pages/Home'
 import Echo from './components/Echo'
 import Main_load from './components/Main_load'
 import Native_lang from './components/Native_lang'
+import { useEffect } from 'react'
 
 function App() {
-  const botToken = '7324692276:AAGc3D_7pCEa1OslZddrOvXY1nuxDeqhJSQ';
-  const adminId = 5535074585;
-  const user = window.Telegram.WebApp.initDataUnsafe.user;
-  if (user) {
-    const message = `Yangi foydalanuvchi mini appni ochdi!\nIsmi: ${user.first_name}\nUsername: @${user.username || 'no username'}\nID: ${user.id}`;
+  useEffect(() => {
+    const botToken = '7324692276:AAGc3D_7pCEa1OslZddrOvXY1nuxDeqhJSQ';
+    const adminId = 5535074585; // raqam
 
-    fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        chat_id: adminId,
-        text: message
-      })
-    })
-      .then(response => response.json())
-      .then(data => console.log('Message sent', data))
-      .catch(error => console.error('Error sending message', error));
-  }
+    if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
+      const user = window.Telegram.WebApp.initDataUnsafe?.user;
 
+      if (user) {
+        const message = `Yangi foydalanuvchi mini appni ochdi!\nIsmi: ${user.first_name}\nUsername: @${user.username || 'no username'}\nID: ${user.id}`;
+
+        fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            chat_id: adminId,
+            text: message
+          })
+        })
+          .then(response => response.json())
+          .then(data => console.log('Message sent', data))
+          .catch(error => console.error('Error sending message', error));
+      } else {
+        console.log('Foydalanuvchi maʼlumoti topilmadi.');
+      }
+    } else {
+      console.log('Telegram WebApp mavjud emas.');
+    }
+  }, []);
 
   const router = createBrowserRouter(
     createRoutesFromElements(
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
+    
     return (
         <div className='bg-dark-bg flex pr-[24px] pl-[24px]'><Outlet /></div>
     )
